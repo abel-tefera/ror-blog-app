b1e3f3644585563b8e690877c4b7b0d1804d9efb
abel-tefera <abeltefera16@gmail.com>
2023-07-12 16:12:43 +0300

    Rubocop fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    belongs_to :user
-    has_many :likes, foreign_key: :post_id
-    has_many :comments, foreign_key: :post_id
+  belongs_to :user
+  has_many :likes, foreign_key: :post_id
+  has_many :comments, foreign_key: :post_id
 end
