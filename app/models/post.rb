e35bf28f897fbdc9d58cbf7eafe406b44f2cc4c4
abel-tefera <abeltefera16@gmail.com>
2023-07-14 15:00:30 +0300

    Tests for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,10 +8,10 @@ class Post < ApplicationRecord
   scope :get_5_comments, ->(post) { post.comments.order('created_at DESC').limit(5) }
 
   validates :title, presence: true
-  validates :author, presence: true
   validates :title, length: { in: 1..250 }
-  validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :author, presence: true
   validates :comment_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
   def update_post_counter
     op = User.find_by_id(author.id)
