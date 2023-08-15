a767c9405f3f567fe03fd9fd0f6aa249663403a0
abel-tefera <abeltefera16@gmail.com>
2023-08-15 09:24:49 +0300

    Updated database to postgre

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_07_12_115424) do
-  
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text "text"
     t.bigint "author_id", null: false
