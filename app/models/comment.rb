fbfd23f0eaf116a7a2dbd1350ea0d4533296274a
abel-tefera <abeltefera16@gmail.com>
2023-08-15 11:32:31 +0300

    Updated scope to instance methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,11 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  before_save :update_comment_counter
+  before_save :update_comments_counter
 
-  def update_comment_counter
+  def update_comments_counter
     post = Post.find_by_id(post_id)
-    post.comment_counter = post.comment_counter.to_i + 1
+    post.comments_counter = post.comments_counter.to_i + 1
     post.save
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,15 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: :post_id
 
   before_save :update_post_counter
-
-  scope :get_5_comments, ->(post) { post.comments.order('created_at DESC').limit(5) }
+  # scope :get_5_comments, ->(post) { post.comments.order('created_at DESC').limit(5) }
 
   def update_post_counter
     op = User.find_by_id(author.id)
     op.posts_counter = op.posts_counter.to_i + 1
     op.save
+  end
+
+  def find_5_comments
+    comments.order('created_at desc').limit(5)
   end
 end
