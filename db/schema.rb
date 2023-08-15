56e9b2e29bcdb37b7fd4cf60e9db50e6140ca025
abel-tefera <abeltefera16@gmail.com>
2023-08-15 09:24:49 +0300

    Updated database to postgre

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_07_12_115424) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text "text"
-    t.integer "author_id", null: false
-    t.integer "post_id", null: false
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
@@ -22,8 +25,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_12_115424) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "author_id", null: false
-    t.integer "post_id", null: false
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_likes_on_author_id"
@@ -35,7 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_12_115424) do
     t.text "text"
     t.integer "comments_counter"
     t.integer "likes_counter"
-    t.integer "author_id", null: false
+    t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
