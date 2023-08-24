2f0ba1508da4b7f56268f8404883703869c35b9f
abel-tefera <abeltefera16@gmail.com>
2023-08-24 12:41:49 +0300

    Created comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  def create
+    @user = ApplicationController.current_user
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.author = @user
+
+    if @comment.save
+      flash[:success] = 'Comment created successfully'
+      redirect_to user_post_path(@user, @post)
+    else
+      flash.now[:errors] = 'Error: Comment could not be saved'
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
