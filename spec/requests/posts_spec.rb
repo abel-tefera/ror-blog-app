4ca0dfd69228da5e5fbaa2e5cb6c69aa64606d72
abel-tefera <abeltefera16@gmail.com>
2023-08-29 13:05:39 +0300

    Updates to tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,35 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  before(:each) do
+    @user = User.create(name: 'Abel')
+    @post = @user.posts.create!(title: 'Hi', text: 'My name is Jeff')
+  end
+
   describe 'GET /index' do
     it 'returns http success' do
-      get '/users/user_id/posts'
+      get user_posts_path(@user)
       expect(response).to have_http_status(:success)
       expect(response.status).to eq(200)
     end
     it 'renders the correct template' do
-      get '/users/user_id/posts'
+      get user_posts_path(@user)
       expect(response).to render_template('index')
-    end
-    it 'response body includes placeholder' do
-      get '/users/user_id/posts'
-      expect(response.body).to include("List of a user's posts")
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/users/user_id/posts/post_id'
+      get user_post_path(@user, @post)
       expect(response).to have_http_status(:success)
       expect(response.status).to eq(200)
     end
     it 'renders the correct template' do
-      get '/users/user_id/posts/post_id'
+      get user_post_path(@user, @post)
       expect(response).to render_template('show')
-    end
-    it 'response body includes placeholder' do
-      get '/users/user_id/posts/post_id'
-      expect(response.body).to include("Showing a user's post")
     end
   end
 end
