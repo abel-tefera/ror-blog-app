ae960b232a2bf8b2da78ebdc6fd2487a5ded9f79
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-08-30 15:00:58 +0200

    Create users tests

--- test/system/user_show_spec.rb
+++ test/system/user_show_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'User show page', type: :feature do
+  user = User.create(name: 'Abel', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+  Post.create(author_id: user.id, title: 'Hello', text: 'This is my first post')
+  Post.create(author_id: user.id, title: 'Hello', text: 'This is my second post')
+  Post.create(author_id: user.id, title: 'Hello', text: 'This is my third post')
+  Post.create(author_id: user.id, title: 'Hello', text: 'This is my fourth post')
 
+  before :each do
+    visit user_path(user)
+  end
 end
