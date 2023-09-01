ba47372b542025a958a1ad05dcf50701f18c0327
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-09-01 15:06:07 +0200

    Fix n+1 issue

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,11 +1,5 @@
 class Api::V1::PostsController < ApplicationController
   def index
     @posts = Post.includes(comments: [:author]).where(author_id: params[:user_id])
-
-    if @posts
-      render json: @posts.to_json, status: :ok
-    else
-      render json: { status: 'Error', errors: @posts.errors }
-    end
-  end
+    render json: @posts, status: :ok  end
 end
